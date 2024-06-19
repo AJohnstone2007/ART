f901e5b501163aa3f86c2781d6675228125567b7
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-06-19 23:42:55 +0100

    New term grammar tested; rewriter not yet reworked

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 60;}
-  public static String timeStamp() {return "2024-06-19 16:42:30";}
+  public static String timeStamp() {return "2024-06-19 23:42:00";}
   public static String version() { return major()+"."+minor()+"."+build() + " " + timeStamp(); };
 }
