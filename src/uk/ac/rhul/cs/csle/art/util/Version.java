510cd933b3908e48c46e19a6da46bacce601b003
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-06-25 21:39:51 +0100

    Reorganisation of directory structure to accomodate old tools

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 63;}
-  public static String timeStamp() {return "2024-06-20 23:43:56";}
+  public static int build() {return 64;}
+  public static String timeStamp() {return "2024-06-25 21:39:12";}
   public static String version() { return major()+"."+minor()+"."+build() + " " + timeStamp(); };
 }
