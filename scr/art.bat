746fc35642573b6ebedc3cdb637dd0ee29fcc15a
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-06-25 23:44:00 +0100

    Move richtext.jar to top level; update art.bat and get.bat. Fix missing .css resource in IDE

--- scr/art.bat
+++ scr/art.bat
@@ -1,2 +1,2 @@
-java --module-path %jfxHome%\lib --add-modules javafx.controls -cp .;%artHome%\art.jar;%artHome%\lib\richtextfx-fat-0.11.2.jar uk.ac.rhul.cs.csle.art.ART %*
+java --module-path %jfxHome%\lib --add-modules javafx.controls -cp .;%artHome%\art.jar;%artHome%\richtextfx.jar uk.ac.rhul.cs.csle.art.ART %*
 
