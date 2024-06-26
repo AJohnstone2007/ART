611f7da1efa73041014ae251c16c969d044df983
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-06-26 09:45:42 +0100

    Add .gitattributes tocontrol language reporting

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 68;}
-  public static String timeStamp() {return "2024-06-26 00:21:07";}
+  public static int build() {return 69;}
+  public static String timeStamp() {return "2024-06-26 09:45:31";}
   public static String version() { return major()+"."+minor()+"."+build() + " " + timeStamp(); };
 }
