3a06fa0f74c6a9857b8dbe8ae785b62c135fbadd
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-06-26 09:28:33 +0100

    Remove rdp and gtb documentation sources

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 67;}
-  public static String timeStamp() {return "2024-06-25 23:42:07";}
+  public static int build() {return 68;}
+  public static String timeStamp() {return "2024-06-26 00:21:07";}
   public static String version() { return major()+"."+minor()+"."+build() + " " + timeStamp(); };
 }
