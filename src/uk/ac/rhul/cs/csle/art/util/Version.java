b72ada9d24fc9ccb0141bda54ba3907d7460cdf0
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-08-21 16:23:48 +0100

    New Jar files compiled under Java 17

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 117;}
-  public static String timeStamp() {return "2024-08-21 15:21:37";}
+  public static int build() {return 119;}
+  public static String timeStamp() {return "2024-08-21 16:23:13";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
