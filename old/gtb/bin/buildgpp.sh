ad3eb4c867e1a4f43ecd936d34aa4a93deb12a92
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-09-02 13:53:41 +0100

    Added buildgpp.sh

--- old/gtb/bin/buildgpp.sh
+++ old/gtb/bin/buildgpp.sh
@@ -1,4 +1,4 @@
-@echo on
+#!/usr/bin/sh
 g++               -fpermissive -O3 -ogtb_mgw32_gcc_O3      -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
 g++ -DBRNGLR_POOL -fpermissive -O3 -ogtb_mgw32_gcc_O3_pool -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
 
