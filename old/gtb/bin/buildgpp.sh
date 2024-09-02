90df6f90e335a0d7ee18a728bc7a0c1e1161626b
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-09-02 21:39:38 +0100

    Add address sanitisation versions to GTB

--- old/gtb/bin/buildgpp.sh
+++ old/gtb/bin/buildgpp.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-g++               -fpermissive -O3 -ogtb_mgw32_gcc_O3      -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
-g++ -DBRNGLR_POOL -fpermissive -O3 -ogtb_mgw32_gcc_O3_pool -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
+
+rm ~/referenceLanguageCorpora/experiments/try/tools/gtb/*.exe
+
+g++                                  -fpermissive -O3 -ogtb_mgw32_gcc_O3               -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
+g++                    -DBRNGLR_POOL -fpermissive -O3 -ogtb_mgw32_gcc_O3_pool          -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
+g++ -fsanitize=address               -fpermissive -O3 -ogtb_mgw32_gcc_O3_sanitize      -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
+g++ -fsanitize=address -DBRNGLR_POOL -fpermissive -O3 -ogtb_mgw32_gcc_O3_pool_sanitize -Wno-write-strings -I../src/gtb -I../src/gtb_lib ../src/gtb/*.cpp ../src/gtb_lib/*.cpp
+
+cp ART/old/gtb/bin/gtb_mgw* ~/referenceLanguageCorpora/experiments/tools/gtb/
+cp ART/old/gtb/bin/gtb_mgw* ~/referenceLanguageCorpora/experiments/try/tools/gtb/
 
