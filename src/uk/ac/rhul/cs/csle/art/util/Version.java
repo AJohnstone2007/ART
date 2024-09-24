75e3f2fc654f750613e98abc6a9931769c6efd2a
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-09-24 18:46:58 +0100

    CFG text pretty print names and expressions complete; annotation not yet done

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 212;}
-  public static String timeStamp() {return "2024-09-24 18:31:43";}
+  public static int build() {return 213;}
+  public static String timeStamp() {return "2024-09-24 18:46:41";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
