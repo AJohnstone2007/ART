65ad767982975bcd46d8796ef0b5d8ba89ba0653
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-09-24 08:06:32 +0100

    Specification grammar updates to allow script printing

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 211;}
-  public static String timeStamp() {return "2024-09-24 07:10:52";}
+  public static String timeStamp() {return "2024-09-24 08:06:24";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
