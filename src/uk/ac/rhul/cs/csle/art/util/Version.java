44991faad4f6b791b67eb02825693a19ca3b5bd5
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-10-21 11:30:24 +0100

    Update of ARTSpecification.art to allow rule printing

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 231;}
-  public static String timeStamp() {return "2024-10-20 22:44:54";}
+  public static int build() {return 232;}
+  public static String timeStamp() {return "2024-10-21 11:30:04";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
