d0e8a9af44df5f81adf721b87afc6e9967c749dd
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-11-11 05:41:25 +0000

    Brute force SPPF cycle breaking: test candidate for EAS

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 248;}
-  public static String timeStamp() {return "2024-11-11 05:38:02";}
+  public static String timeStamp() {return "2024-11-11 05:41:16";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
