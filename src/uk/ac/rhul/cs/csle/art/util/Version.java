0ce88d45dc95fcefd06909925b25706e8ae7ae71
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2024-11-27 12:23:36 +0000

    alpha to beta commit

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 262;}
-  public static String timeStamp() {return "2024-11-27 12:22:09";}
+  public static String timeStamp() {return "2024-11-27 12:23:18";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
