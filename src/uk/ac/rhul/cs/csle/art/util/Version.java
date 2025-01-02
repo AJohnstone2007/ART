565b47c2c073dcd655914bffde903eab5f38cfb6
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-01-02 22:31:11 +0000

    !print sentences Q&D test candidate for EAS

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 319;}
-  public static String timeStamp() {return "2025-01-02 22:26:34";}
+  public static int build() {return 320;}
+  public static String timeStamp() {return "2025-01-02 22:31:02";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
