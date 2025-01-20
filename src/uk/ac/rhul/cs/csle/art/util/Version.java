04e0dd56567486258206a4e7b60091d378a843e7
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-01-20 13:47:43 +0000

    Update slewa.pdf

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 346;}
-  public static String timeStamp() {return "2025-01-20 07:47:32";}
+  public static int build() {return 347;}
+  public static String timeStamp() {return "2025-01-20 13:47:31";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
