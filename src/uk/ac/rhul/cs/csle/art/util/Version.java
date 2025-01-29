d2c4dbed63eb5fbf824f3b7fc8a0fd154271e3a7
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-01-29 15:38:08 +0000

    Update section heading in C.2

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 388;}
-  public static String timeStamp() {return "2025-01-29 15:33:42";}
+  public static String timeStamp() {return "2025-01-29 15:37:57";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
