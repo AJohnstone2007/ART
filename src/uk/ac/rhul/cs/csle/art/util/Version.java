5102437bc8d0c9e1054f43651bb13a3c852a9040
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-01-30 23:42:09 +0000

    Corrected cyclic node detection

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 395;}
-  public static String timeStamp() {return "2025-01-30 23:39:06";}
+  public static int build() {return 396;}
+  public static String timeStamp() {return "2025-01-30 23:42:00";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
