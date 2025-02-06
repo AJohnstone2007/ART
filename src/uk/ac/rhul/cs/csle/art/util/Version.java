0ebe28939e0675446258f24255b295c1512a8b67
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-02-06 00:15:18 +0000

    Small tweaks to !print

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 421;}
-  public static String timeStamp() {return "2025-02-06 00:09:38";}
+  public static int build() {return 422;}
+  public static String timeStamp() {return "2025-02-06 00:15:09";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
