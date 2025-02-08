7ef381796d688b82a8688c73253dedf40a8e2268
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-02-08 07:48:19 +0000

    Rework script syntax for !configuration

--- src/uk/ac/rhul/cs/csle/art/interpret/eSOSInterpreter.java
+++ src/uk/ac/rhul/cs/csle/art/interpret/eSOSInterpreter.java
@@ -1,7 +1,16 @@
 package uk.ac.rhul.cs.csle.art.interpret;
 
+import uk.ac.rhul.cs.csle.art.cfg.AbstractParser;
+
 public class eSOSInterpreter extends AbstractInterpreter {
+  private AbstractParser parser;
+
   public eSOSInterpreter() {
     System.out.println("Interpreter set to eSOS");
+  }
+
+  @Override
+  public void interpret(AbstractParser parser) {
+    this.parser = parser;
   }
 }
--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 429;}
-  public static String timeStamp() {return "2025-02-07 21:58:52";}
+  public static int build() {return 430;}
+  public static String timeStamp() {return "2025-02-08 07:47:56";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
