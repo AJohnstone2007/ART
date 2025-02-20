b0f03c0fd1e4fe26b60c1f3a9eff9447801cdd06
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-02-20 08:35:51 +0000

    Tweaks to lab 5

--- doc/project/music/runReduction.bat
+++ doc/project/music/runReduction.bat
@@ -1,3 +1,3 @@
-javac -cp .;art.jar ARTVAluePlugin.java
-java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART reduction.art
+javac -cp .;art.jar ARTValuePlugin.java
+java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART Reduction.art
 
