dafd81599a115764cff9d3b9d94f0af60d94cd8d
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-02-23 15:29:18 +0000

    Suppress slewa.pdf

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 497;}
-  public static String timeStamp() {return "2025-02-23 15:22:22";}
+  public static String timeStamp() {return "2025-02-23 15:28:57";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
