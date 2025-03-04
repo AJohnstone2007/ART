473b71e623603098319c9d5ec71ea67b744aebac
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-03-04 08:45:10 +0000

    Recycle slewa

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 519;}
-  public static String timeStamp() {return "2025-03-04 08:41:01";}
+  public static String timeStamp() {return "2025-03-04 08:44:59";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
