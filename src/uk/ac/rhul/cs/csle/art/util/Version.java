7e29ef52bac0fcc0355b7a974a3899efb88ea4ee
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-03-06 08:04:47 +0000

    Update lab 6

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 521;}
-  public static String timeStamp() {return "2025-03-06 07:21:44";}
+  public static String timeStamp() {return "2025-03-06 08:04:36";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
