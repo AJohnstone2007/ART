05f8a85b5a328bf2597e69ac4c9507c90228a568
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-03-13 08:02:12 +0000

    Update mini

---
+++ doc/examples/mini/runMini.sh
@@ -0,0 +1,10 @@
+javac -cp .:art.jar ARTValuePlugin.java
+java  -cp .:art.jar uk.ac.rhul.cs.csle.art.ART $1 !generate actions
+javac -cp .:art.jar ARTGeneratedActions.java
+java  -cp .:art.jar uk.ac.rhul.cs.csle.art.ART !interpreter attributeAction $1
+
+
+
+
+
+
