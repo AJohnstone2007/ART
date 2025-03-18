e81c443abda3631cefcd9fbe3b8fbccc3cb83a3a
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-03-18 08:27:59 +0000

    Add coda.pdf slides

--- src/ARTGeneratedActions.java
+++ src/ARTGeneratedActions.java
@@ -6,7 +6,7 @@ import uk.ac.rhul.cs.csle.art.util.Util;
 public class ARTGeneratedActions extends AbstractActions {
  Map<String, Integer> variables = new HashMap<>(); 
    Map<String, AbstractAttributeBlock> procedures = new HashMap<>(); 
-  public String name() { return "2025-03-17 18:42:17"; }
+  public String name() { return "2025-03-17 22:23:10"; }
 
   public class ART_C_ID extends AbstractAttributeBlock {
     ART_C_ID ID = this; String v;
