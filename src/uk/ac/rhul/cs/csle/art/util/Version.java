ef20720186a3efa81c00071e50b709dc7847a54a
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-06-24 08:13:49 +0100

    SML tidying

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 657;}
-  public static String timeStamp() {return "2025-06-24 08:03:30";}
+  public static int build() {return 658;}
+  public static String timeStamp() {return "2025-06-24 08:13:08";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
