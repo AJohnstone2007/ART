4f35093fe0d5e85fe833613ab19bd459acf6d061
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-07-11 14:38:19 +0100

    TWEset indicesnow 1..n, but leading whitespace is broken

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 667;}
-  public static String timeStamp() {return "2025-07-11 14:35:34";}
+  public static String timeStamp() {return "2025-07-11 14:38:06";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
