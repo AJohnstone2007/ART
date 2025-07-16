fcbf983a72b699c01f52295e1d35d8752c921818
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-07-16 22:31:32 +0100

    Alpha publish

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 671;}
-  public static String timeStamp() {return "2025-07-16 22:29:13";}
+  public static int build() {return 672;}
+  public static String timeStamp() {return "2025-07-16 22:31:19";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
