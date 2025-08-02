c1879c0b6b575bee1132cef669531ec3690d2a7f
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-08-02 14:39:13 +0100

    Add !token directive

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 708;}
-  public static String timeStamp() {return "2025-08-02 06:55:37";}
+  public static int build() {return 709;}
+  public static String timeStamp() {return "2025-08-02 14:38:56";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
