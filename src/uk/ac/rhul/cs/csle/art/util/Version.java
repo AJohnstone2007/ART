fd20678aa5c653029f9c1bba62ce8d951df5f392
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-08-13 10:33:02 +0100

    Change !restore to !recall

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 719;}
-  public static String timeStamp() {return "2025-08-13 10:24:39";}
+  public static int build() {return 720;}
+  public static String timeStamp() {return "2025-08-13 10:32:48";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
