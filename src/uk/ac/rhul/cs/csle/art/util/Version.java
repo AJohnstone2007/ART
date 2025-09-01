914d5f305b1b7b6a0ca983bbe444847b7f114eb6
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-09-01 20:41:11 +0100

    New richtextfx distribution

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 758;}
-  public static String timeStamp() {return "2025-09-01 20:35:15";}
+  public static int build() {return 759;}
+  public static String timeStamp() {return "2025-09-01 20:40:59";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
