0645a1fa7416a2d97a2f6445b78721c59c3fbbd7
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-09-02 14:46:41 +0100

    Separated derivation and lexical chooser. Lexical load and print correctly but choice not working

--- src/uk/ac/rhul/cs/csle/art/choose/ChooseRules.java
+++ src/uk/ac/rhul/cs/csle/art/choose/ChooseRules.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import uk.ac.rhul.cs.csle.art.cfg.cfgRules.CFGElement;
 import uk.ac.rhul.cs.csle.art.cfg.cfgRules.CFGKind;
+import uk.ac.rhul.cs.csle.art.cfg.cfgRules.CFGNode;
 import uk.ac.rhul.cs.csle.art.script.ScriptInterpreter;
 import uk.ac.rhul.cs.csle.art.term.TermTraverserText;
 import uk.ac.rhul.cs.csle.art.util.DisplayInterface;
@@ -14,42 +15,55 @@ import uk.ac.rhul.cs.csle.art.util.relation.Relation;
 import uk.ac.rhul.cs.csle.art.util.statistics.Statistics;
 
 public class ChooseRules implements DisplayInterface {
-  private final Relation<CFGElement, CFGElement> higher;
-  private final Relation<CFGElement, CFGElement> longer;
-  private final Relation<CFGElement, CFGElement> shorter;
+  private final Relation<CFGElement, CFGElement> lexicalHigher;
+  private final Relation<CFGElement, CFGElement> lexicalLonger;
+  private final Relation<CFGElement, CFGElement> lexicalShorter;
+  private final Relation<CFGNode, CFGNode> derivationHigher;
+  private final Relation<CFGNode, CFGNode> derivationLonger;
+  private final Relation<CFGNode, CFGNode> derivationShorter;
 
   public ChooseRules(ChooseRules payload) { // Copy constructor
-    higher = new Relation<>(payload.higher);
-    longer = new Relation<>(payload.longer);
-    shorter = new Relation<>(payload.shorter);
+    lexicalHigher = new Relation<>(payload.lexicalHigher);
+    lexicalLonger = new Relation<>(payload.lexicalLonger);
+    lexicalShorter = new Relation<>(payload.lexicalShorter);
+    derivationHigher = new Relation<>(payload.derivationHigher);
+    derivationLonger = new Relation<>(payload.derivationLonger);
+    derivationShorter = new Relation<>(payload.derivationShorter);
   }
 
   public ChooseRules() {
     super();
-    higher = new Relation<>();
-    longer = new Relation<>();
-    shorter = new Relation<>();
+    lexicalHigher = new Relation<>();
+    lexicalLonger = new Relation<>();
+    lexicalShorter = new Relation<>();
+    derivationHigher = new Relation<>();
+    derivationLonger = new Relation<>();
+    derivationShorter = new Relation<>();
   }
 
-  public void buildChooseRule(int term) {
-    Util.debug("Choose rule : " + ScriptInterpreter.iTerms.toRawString(term));
+  public void buildDerivationChooseRule(int term) {
+    Util.debug("Choose derivation rule : " + ScriptInterpreter.iTerms.toRawString(term));
+  }
+
+  public void buildLexicalChooseRule(int term) {
+    Util.debug("Choose lexical rule : " + ScriptInterpreter.iTerms.toRawString(term));
     ScriptInterpreter.seenChooseRule = true;
     Set<CFGElement> lhs = new HashSet<>(), rhs = new HashSet<>();
-    buildChooseSet(lhs, ScriptInterpreter.iTerms.subterm(term, 0));
-    buildChooseSet(rhs, ScriptInterpreter.iTerms.subterm(term, 2));
+    buildLexicalChooseSet(lhs, ScriptInterpreter.iTerms.subterm(term, 0));
+    buildLexicalChooseSet(rhs, ScriptInterpreter.iTerms.subterm(term, 2));
     var op = ScriptInterpreter.iTerms.termSymbolString(ScriptInterpreter.iTerms.subterm(term, 1));
     switch (op) {
     case "chooseHigher":
-      higher.addAllAll(lhs, rhs);
+      lexicalHigher.addAllAll(lhs, rhs);
       break;
     case "chooseLower":
-      higher.addAllAll(rhs, lhs);
+      lexicalHigher.addAllAll(rhs, lhs);
       break;
     case "chooseLonger":
-      longer.addAllAll(lhs, rhs);
+      lexicalLonger.addAllAll(lhs, rhs);
       break;
     case "chooseShorter":
-      shorter.addAllAll(lhs, rhs);
+      lexicalShorter.addAllAll(lhs, rhs);
       break;
     default:
       Util.fatal("Unexpected chooserOp: " + op);
@@ -57,18 +71,30 @@ public class ChooseRules implements DisplayInterface {
   }
 
   public boolean testHigher(CFGElement left, CFGElement right) {
-    return higher.get(left).contains(right);
+    return lexicalHigher.get(left).contains(right);
   }
 
   public boolean testLonger(CFGElement left, CFGElement right) {
-    return longer.get(left).contains(right);
+    return lexicalLonger.get(left).contains(right);
   }
 
   public boolean testShorter(CFGElement left, CFGElement right) {
-    return shorter.get(left).contains(right);
+    return lexicalShorter.get(left).contains(right);
   }
 
-  void buildChooseSet(Set<CFGElement> set, int term) {
+  public boolean testHigher(CFGNode left, CFGNode right) {
+    return derivationHigher.get(left).contains(right);
+  }
+
+  public boolean testLonger(CFGNode left, CFGNode right) {
+    return derivationLonger.get(left).contains(right);
+  }
+
+  public boolean testShorter(CFGNode left, CFGNode right) {
+    return derivationShorter.get(left).contains(right);
+  }
+
+  void buildLexicalChooseSet(Set<CFGElement> set, int term) {
     int firstChild = ScriptInterpreter.iTerms.subterm(term, 0);
     if (ScriptInterpreter.iTerms.termSymbolString(firstChild).equals("chooseSet")) {
       Set<CFGKind> collectKinds = new HashSet<>();
@@ -113,8 +139,15 @@ public class ChooseRules implements DisplayInterface {
     }
   }
 
-  public void printRelation(Relation<CFGElement, CFGElement> relation, String op, PrintStream outputStream, TermTraverserText outputTraverser, boolean indexed,
-      boolean full, boolean indented) {
+  public void printLexicalRelation(Relation<CFGElement, CFGElement> relation, String op, PrintStream outputStream, TermTraverserText outputTraverser,
+      boolean indexed, boolean full, boolean indented) {
+    for (var t1 : relation.getDomain())
+      for (var t2 : relation.get(t1))
+        outputStream.println(t1 + " " + op + " " + t2);
+  }
+
+  public void printDerivationRelation(Relation<CFGNode, CFGNode> relation, String op, PrintStream outputStream, TermTraverserText outputTraverser,
+      boolean indexed, boolean full, boolean indented) {
     for (var t1 : relation.getDomain())
       for (var t2 : relation.get(t1))
         outputStream.println(t1 + " " + op + " " + t2);
@@ -122,9 +155,12 @@ public class ChooseRules implements DisplayInterface {
 
   @Override
   public void print(PrintStream outputStream, TermTraverserText outputTraverser, boolean indexed, boolean full, boolean indented) {
-    printRelation(higher, ">", outputStream, outputTraverser, indexed, full, indented);
-    printRelation(longer, ">>", outputStream, outputTraverser, indexed, full, indented);
-    printRelation(shorter, "<<", outputStream, outputTraverser, indexed, full, indented);
+    printLexicalRelation(lexicalHigher, ">", outputStream, outputTraverser, indexed, full, indented);
+    printDerivationRelation(derivationHigher, ">", outputStream, outputTraverser, indexed, full, indented);
+    printLexicalRelation(lexicalLonger, ">>", outputStream, outputTraverser, indexed, full, indented);
+    printDerivationRelation(derivationLonger, ">>", outputStream, outputTraverser, indexed, full, indented);
+    printLexicalRelation(lexicalShorter, "<<", outputStream, outputTraverser, indexed, full, indented);
+    printDerivationRelation(derivationShorter, "<<", outputStream, outputTraverser, indexed, full, indented);
   }
 
   @Override
--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 761;}
-  public static String timeStamp() {return "2025-09-01 20:49:07";}
+  public static int build() {return 762;}
+  public static String timeStamp() {return "2025-09-02 14:46:23";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
