238a3bfe9891f762af4c7cae3f6e11bdbb1b139f
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-09-17 18:39:49 +0100

    Clean pshAlpha warnings

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 823;}
-  public static String timeStamp() {return "2025-09-17 18:36:50";}
+  public static int build() {return 824;}
+  public static String timeStamp() {return "2025-09-17 18:39:40";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
