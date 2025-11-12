3fe87b8effeee7164b7579c70640f72076572744
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-11-12 21:56:09 +0000

    Suppress interpreterTerm.dot

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 881;}
-  public static String timeStamp() {return "2025-11-12 19:57:50";}
+  public static int build() {return 882;}
+  public static String timeStamp() {return "2025-11-12 21:55:57";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
