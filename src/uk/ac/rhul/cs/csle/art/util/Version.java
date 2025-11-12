6865526ade015ca7e7097d5892eebf7a8679fa3e
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2025-11-12 17:20:00 +0000

    Fixed lexeme for STRING_BRACE_NEST

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 879;}
-  public static String timeStamp() {return "2025-11-12 17:18:30";}
+  public static int build() {return 880;}
+  public static String timeStamp() {return "2025-11-12 17:19:49";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
