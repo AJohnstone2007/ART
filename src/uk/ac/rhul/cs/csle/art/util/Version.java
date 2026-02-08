ac1a993e7a494882444f481d6f144f0786110115
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-02-08 18:48:28 +0000

    Update slewa reduction trace text

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -3,6 +3,6 @@ public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
   public static int build() {return 904;}
-  public static String timeStamp() {return "2026-02-07 22:08:24";}
+  public static String timeStamp() {return "2026-02-08 18:47:35";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
