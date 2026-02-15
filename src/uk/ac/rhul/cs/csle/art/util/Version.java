7a65e1e91f421ecd3fa1fb3e36e2237c9bd91b91
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-02-15 03:16:49 +0000

    Chang attribute type designator to ::

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 910;}
-  public static String timeStamp() {return "2026-02-13 22:06:58";}
+  public static int build() {return 911;}
+  public static String timeStamp() {return "2026-02-15 03:16:15";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
