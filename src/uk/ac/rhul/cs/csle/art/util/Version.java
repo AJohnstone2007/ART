165a4e0c1386a309e326b8ff9bdc46510d6f5606
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-02-16 10:11:48 +0000

    Add runAttribute.bat to examples directory

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 913;}
-  public static String timeStamp() {return "2026-02-16 09:12:27";}
+  public static int build() {return 914;}
+  public static String timeStamp() {return "2026-02-16 10:11:33";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
