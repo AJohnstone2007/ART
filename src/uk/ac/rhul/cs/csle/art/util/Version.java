e119ecbe62404567a3a2abd62305d7fef1273f9e
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-02-22 18:27:45 +0000

    Update SLE26Examples.art

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 921;}
-  public static String timeStamp() {return "2026-02-22 17:10:43";}
+  public static int build() {return 923;}
+  public static String timeStamp() {return "2026-02-22 18:27:14";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
