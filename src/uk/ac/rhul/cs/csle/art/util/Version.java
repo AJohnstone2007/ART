9020a4510793c9d1d3965ed99feaf763403d2927
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-03-12 10:32:54 +0000

    Mini \n update

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 946;}
-  public static String timeStamp() {return "2026-03-12 08:51:24";}
+  public static int build() {return 947;}
+  public static String timeStamp() {return "2026-03-12 10:32:36";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
