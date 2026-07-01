6b8e2afe0ff3797fafefeb240c063f2a7f631f6d
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-07-01 05:36:09 +0100

    Dev checkpoint en route to new directive regime.

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 959;}
-  public static String timeStamp() {return "2026-07-01 05:18:22";}
+  public static int build() {return 960;}
+  public static String timeStamp() {return "2026-07-01 05:35:50";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
