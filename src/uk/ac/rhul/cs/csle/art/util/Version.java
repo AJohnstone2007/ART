d9cded9c7641e4b3520d815c99b52961a3f75c9e
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-07-14 10:09:45 +0100

    Test version for EAS with corrected parser

--- src/uk/ac/rhul/cs/csle/art/util/Version.java
+++ src/uk/ac/rhul/cs/csle/art/util/Version.java
@@ -2,7 +2,7 @@ package uk.ac.rhul.cs.csle.art.util;
 public class Version {
   public static int major() {return 5;}
   public static int minor() {return 0;}
-  public static int build() {return 967;}
-  public static String timeStamp() {return "2026-07-14 10:04:07";}
+  public static int build() {return 968;}
+  public static String timeStamp() {return "2026-07-14 10:09:19";}
   public static String version() { return major()+"_"+minor()+"_"+build() + " " + timeStamp(); };
 }
